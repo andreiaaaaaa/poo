5d5b99c710d72656ee476a9e20349bd635b185aa
andreiaaaaaa <andreia.alves@escolar.ifrn.edu.br>
2025-09-25 11:44:14 +0000

    oi

---
+++ Agenda/index.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("POO em Python com Streamlit")
+
+if st.button("Clique Aqui"):
+    st.write("Bem-vindo(a) ao Streamlit")
--- Agenda/models/index.py
+++
@@ -1,7 +0,0 @@
-from templates.ManterClienteUI import ManterClienteUI
-
-class IndexUI:
-    def main():
-        ManterClienteUI.main()
-
-IndexUI.main()
