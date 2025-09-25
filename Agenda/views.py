faf29ebff13d8598615616ff2fa58b014f572189
andreiaaaaaa <andreia.alves@escolar.ifrn.edu.br>
2025-09-25 12:04:06 +0000

    oi

--- Agenda/views.py
+++ Agenda/views.py
@@ -1,5 +1,7 @@
 from models.cliente import Cliente
 from models.clienteDAO import ClienteDAO
+from models.servico import Servico
+from models.servicoDAO import ServicoDAO
 
 
 class View:
@@ -21,3 +23,21 @@ class View:
     def cliente_excluir(id):
         cliente = Cliente(id, "", "", "")
         ClienteDAO.excluir(cliente)
+
+    def servico_listar():
+        return ServicoDAO.listar()
+
+    def servico_listar_id(id):
+        return ServicoDAO.listar_id(id)
+
+    def servico_inserir(descricao, valor):
+        servico = Servico(0, descricao, valor)
+        ServicoDAO.inserir(servico)
+
+    def servico_atualizar(id, descricao, valor):
+        servico = Servico(id, descricao, valor)
+        ServicoDAO.atualizar(servico)
+
+    def servico_excluir(id):
+        servico = Servico(id, "", 0.0)
+        ServicoDAO.excluir(servico)
