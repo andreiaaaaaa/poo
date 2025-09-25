0ab374d9d7d483a7736257f34aee9ef791a2164f
andreiaaaaaa <andreia.alves@escolar.ifrn.edu.br>
2025-09-25 12:03:22 +0000

    oi

--- Agenda/index.py
+++ Agenda/index.py
@@ -1,7 +1,16 @@
 from templates.ManterClienteUI import ManterClienteUI
+from templates.ManterServicoUI import ManterServicoUI
+
+import streamlit as st
 
 class IndexUI:
     def main():
-        ManterClienteUI.main()
+        st.sidebar.title("Menu")
+        op = st.sidebar.selectbox("Escolha uma opção", ["Clientes", "Serviços"])
+
+        if op == "Clientes":
+            ManterClienteUI.main()
+        elif op == "Serviços":
+            ManterServicoUI.main()
 
 IndexUI.main()
