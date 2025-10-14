f0821b0db31b097cf08eb8e156ee8b4f72d3c49c
andreiaaaaaa <andreia.alves@escolar.ifrn.edu.br>
2025-10-14 14:33:15 +0000

    oi

--- Agenda/templates/ManterClienteUI.py
+++ Agenda/templates/ManterClienteUI.py
@@ -4,7 +4,6 @@ from views import View
 
 class ManterClienteUI:
     def main():
-        st.header("Cadastro de Clientes")
 
         tab1, tab2, tab3, tab4 = st.tabs(["Listar", "Inserir", "Atualizar", "Excluir"])
 
