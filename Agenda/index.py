40204feb69d9e8c6c4c9fca70d8c0b2f7ecee0d5
andreiaaaaaa <andreia.alves@escolar.ifrn.edu.br>
2025-10-30 13:03:32 +0000

    oii

--- Agenda/index.py
+++ Agenda/index.py
@@ -6,6 +6,7 @@ from templates.manterClienteUI import ManterClienteUI
 from templates.manterServicoUI import ManterServicoUI
 from templates.manterHorarioUI import ManterHorarioUI
 from templates.manterProfissionalUI import ManterProfissionalUI
+from views import View
 
 
 class IndexUI:
@@ -31,5 +32,26 @@ class IndexUI:
         if op == "Cadastro de Serviços": ManterServicoUI.main()
         if op == "Cadastro de Profissionais": ManterProfissionalUI.main()
         if op == "Cadastro de Horários": ManterHorarioUI.main()
+
+    def sair_do_sistema():
+        if st.sidebar.button("Sair"):
+            del st.session_state["usuario_id"]
+            del st.session_state["usuario_nome"]
+            st.rerun()
+
+    def sidebar():
+        if "usuario_id" not in st.session_state:
+            IndexUI.menu_visitante()
+        else:
+            admin = st.session_state["usuario_nome"] == "admin"
+            st.sidebar.write("Bem-vindo(a), " + st.session_state["usuario_nome"])
+            if admin: IndexUI.menu_admin()
+            else: IndexUI.menu_cliente()
+            IndexUI.sair_do_sistema()
+
+    def main():
+        View.cliente_criar_admin()  
+        IndexUI.sidebar()
+
 IndexUI.menu_admin()
         
